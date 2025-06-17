eb862c66428714952b23a5a403a3e2fa06d4abf2
Prateek Patel <prateekpro0803@gmail.com>
2025-06-17 19:08:36 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which CSS property is used is used to control the spacing between elements?",
+    answer: "Padding",
+    options: [
+      "Margin",
+      "Padding",
+      "Spacing",
+      "Border-spacing"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the Javascript function used to select an HTML element by its id?",
+    answer: "getElementById",
+    options: [
+      "document.query",
+      "getElementById",
+      "selectElement",
+      "findElementById"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
